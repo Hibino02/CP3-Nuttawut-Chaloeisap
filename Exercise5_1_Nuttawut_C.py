3b4bee5678d490515c3144c4cdd9ab6e65c94ee1
Hibino02 <zolo_atm@msn.com>
2021-06-20 15:33:09 +0700

    Exercise5_1_Nuttawut_C

---
+++ Exercise5_1_Nuttawut_C.py
@@ -0,0 +1,10 @@
+a = int(input("First Number :"))
+b = int(input("Second  Number :"))
+r1 = a+b
+r2 = a-b
+r3 = a*b
+r4 = a/b
+print("result a+b : ",r1)
+print("result a-b : ",r2)
+print("result a*b : ",r3)
+print("result a/b : ",r4)
